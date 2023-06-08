31f0ac6d2d4771bdaec4196a815f7816a6e288bb
manishaac2002 <manishaac2002@gmail.com>
2023-06-08 10:22:30 +0100

    feat: credential details

--- server.js
+++ server.js
@@ -1,7 +1,28 @@
 import express from 'express';
 import multer from 'multer';
+//to connecting aws to upload the file in s3 bucket
+import { S3Client, PutObjectCommand} from "@aws-sdk/client-s3";
+import dotenv from 'dotenv'
 
 const application = express()
+
+dotenv.config()
+
+// creating a variables for credentials in env
+const bucketName = process.env.BUCKET_NAME
+const bucketRegion = process.env.BUCKET_REGION
+const accessKey = process.env.ACCESS_KEY
+const secretAccessKey = process.env.SECRET_ACCESS_KEY
+console.log(bucketName);
+// credential details
+const s3 = new S3Client({
+    credentials: {
+        accessKeyID: accessKey,
+        secretAccessKey: secretAccessKey
+    },
+    region: bucketRegion
+})
+
 
 // storage engine 
 const fileStorage = multer.diskStorage({
@@ -22,25 +43,35 @@ const fileStorage = multer.diskStorage({
 const fileUpload = multer({ storage: fileStorage })
 
 // to upload the single file route
-application.post("/single", fileUpload.single("image"), (request, response) => {
+application.post("/single", fileUpload.single("image"), async(request, response) => {
     console.log(request.file);
+    const params = {
+        Bucket:bucketName,
+        Key:request.file.originalname,
+        Body:request.file.buffer,
+        Content:request.file.mimetype
+    }
+    
+    const command =new PutObjectCommand(params)
+    await s3.send(command)
+
     response.send("Single file uploaded successfully");
 })
 
 // to upload multiple file uploads
-application.post("/multipleFile", fileUpload.array("image",3), (request, response) => {
+application.post("/multipleFile", fileUpload.array("image", 3), (request, response) => {
     console.log(request.files);
     // it returns in this form while print the file inn print statement
     // {
-//     fieldname: 'image',
-//     originalname: 'Bollywood Snapped_ Photo.jpg',
-    // encoding: '7bit',//it convert into code form
-//     mimetype: 'image/jpeg',
-//     destination: './images',
-//     filename: 'f73c49b6f4925c298a054b5394ff4f09',
-//     path: 'images\\f73c49b6f4925c298a054b5394ff4f09',
-//     size: 96854
-//   },
+    //     fieldname   : 'image',
+    //     originalname: 'Bollywood Snapped_ Photo.jpg',
+    //     encoding    : '7bit',//it convert into code form
+    //     mimetype    : 'image/jpeg',
+    //     destination : './images',
+    //     filename    : 'f73c49b6f4925c298a054b5394ff4f09',
+    //     path        : 'images\\f73c49b6f4925c298a054b5394ff4f09',
+    //     size        :  96854
+    //   },
     response.send("Multiple file uploaded successfully")
 })
 
